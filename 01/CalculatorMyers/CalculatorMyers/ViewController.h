e7fedf338a71d5aa8856ab427d21f9530ed6d192
Chris Myers <cmyers78@gmail.com>
2016-05-23 13:38:43 -0600

    Adding calculator assignment

---
+++ 01/CalculatorMyers/CalculatorMyers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CalculatorMyers
+//
+//  Created by Christopher Myers on 5/23/16.
+//  Copyright © 2016 DragomanDevelopers, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 01/CalculatorMyers/CalculatorMyers/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CalculatorMyers
+//
+//  Created by Christopher Myers on 5/23/16.
+//  Copyright © 2016 DragomanDevelopers, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
