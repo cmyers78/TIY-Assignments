c11bac122b6927c33be101e9ee364f5963215ea3
Chris Myers <cmyers78@gmail.com>
2016-05-24 15:00:40 -0600

    Working...right now!!!

--- 02/AgentAppMyers/AgentAppMyers/ViewController.m
+++ 02/AgentAppMyers/AgentAppMyers/ViewController.m
@@ -8,24 +8,61 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    NSString *agentName;
+    NSString *password;
+    
+}
+
 @property (weak, nonatomic) IBOutlet UITextField *userNameTextField;
 
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
 
 @property (weak, nonatomic) IBOutlet UIButton *authenticateButton;
+
+@property (weak, nonatomic) IBOutlet UILabel *welcomeLabel;
 
 @end
 
 @implementation ViewController
 
+
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    agentName = @"Christopher Myers";
+    password = @"123456";
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)authenticate {
+    
+    if ([self.userNameTextField.text isEqualToString:agentName] && [self.passwordTextField.text isEqualToString:password]) {
+        
+        self.view.backgroundColor = [UIColor greenColor];
+        self.welcomeLabel.text = [NSString stringWithFormat:@"Welcome %@", agentName];
+        
+        
+    } else {
+        self.view.backgroundColor = [UIColor redColor];
+        self.welcomeLabel.text = [NSString stringWithFormat:@"DENIED"];
+        
+        
+    }
+
+    
 }
+
+- (IBAction)authenticateButton:(UIButton *)sender {
+
+    
+    [self authenticate];
+    
+    
+    
+}
+
 
 @end
