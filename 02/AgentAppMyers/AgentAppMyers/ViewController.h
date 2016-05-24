927844fa8edb0db3be0f131323f4c4233f565d88
Chris Myers <cmyers78@gmail.com>
2016-05-24 13:33:56 -0600

    Storyboard view updated - part 1

---
+++ 02/AgentAppMyers/AgentAppMyers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AgentAppMyers
+//
+//  Created by Christopher Myers on 5/24/16.
+//  Copyright © 2016 DragomanDevelopers, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 02/AgentAppMyers/AgentAppMyers/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  AgentAppMyers
+//
+//  Created by Christopher Myers on 5/24/16.
+//  Copyright © 2016 DragomanDevelopers, LLC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *userNameTextField;
+
+
+@property (weak, nonatomic) IBOutlet UIButton *authenticateButton;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
