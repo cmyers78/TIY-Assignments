d9e927304f4ca64bf258b9520dde4095b13df1ed
Chris Myers <cmyers78@gmail.com>
2016-06-11 14:42:41 -0600

    3rd Commit - 70% complete - restaurant data populated into cells.

--- 14/Gastronomy Myers/Gastronomy Myers/RestaurantListViewController.m
+++ 14/Gastronomy Myers/Gastronomy Myers/RestaurantListViewController.m
@@ -7,25 +7,54 @@
 //
 
 #import "RestaurantListViewController.h"
+#import "Restaurant.h"
+#import "Dishes.h"
 
-@interface RestaurantListViewController ()
+
+@interface RestaurantListViewController () <UITableViewDelegate, UITableViewDataSource>
+
 @property (weak, nonatomic) IBOutlet UITableView *restaurantTableView;
+
+@property (strong, nonatomic) NSMutableArray * restaurantArray;
+
+@property (strong, nonatomic) Restaurant * currentRestaurant;
 
 @end
 
 @implementation RestaurantListViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+
+- (void) viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    self.restaurantArray = [[NSMutableArray alloc] init];
+    NSDictionary * theDictonary = [self parseRawJSON];
+    //NSLog(@"the dictionary: %@", theDictonary);
+    
+    [self parseRefinedJSON:theDictonary];
+    
+    //NSLog(@"the number of restaurants is: %ld", self.restaurantArray.count);
+    
+
+}
+#pragma mark - Delegate methods
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return [self.restaurantArray count];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"restCell" forIndexPath:indexPath];
+    
+    self.currentRestaurant = [self.restaurantArray objectAtIndex:indexPath.row];
+    
+    cell.textLabel.text = self.currentRestaurant.restName;
+    
+    return cell;
 }
 
-/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -33,6 +62,170 @@
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
-*/
+
+
+- (NSDictionary *) parseRawJSON {
+    // load JSON data into NSString
+    // Get URLfor the file
+    NSString * filePath = [[NSBundle mainBundle] pathForResource:@"restaurantData" ofType:@"json"];
+    // load contents of file into an NSString
+    NSString * jsonString = [[NSString alloc] initWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:NULL];
+    
+    // Convert string into data object
+    NSData * data = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+    
+    if (data == nil) {
+        NSLog(@"There was a problem loading the data");
+    }
+    // Create NSError
+    NSError * jsonError = nil;
+    
+    //Convert NSData to NSDiction using JSONSerialization
+    NSDictionary * jsonDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&jsonError];
+    
+    return jsonDictionary;
+}
+
+
+-(void) parseRefinedJSON: (NSDictionary *) rawDictonary {
+    
+    NSArray * jsonArray = [rawDictonary objectForKey:@"restaurants"];
+    
+    for (NSDictionary * dict in jsonArray) {
+        
+        Restaurant * theRestaurant = [[Restaurant alloc] init];
+        
+        NSString * name = [dict objectForKey:@"name"];
+        
+        if (name != nil) {
+            theRestaurant.restName = name;
+        } else {
+            theRestaurant.restName = @"";
+            NSLog(@"I could not parse the name.");
+        }
+        
+        NSString * desc = [dict objectForKey:@"description"];
+        if (desc != nil) {
+            theRestaurant.restDescription = desc;
+        } else {
+            theRestaurant.restDescription = @"";
+            NSLog(@"I could not parse the restaurant description");
+        }
+        
+        NSString * imageString = [dict objectForKey:@"imageNameString"];
+        if (imageString != nil) {
+            theRestaurant.restImageString = imageString;
+        } else {
+            theRestaurant.restImageString = @"";
+            NSLog(@"I could not parse the restaurant image string");
+        }
+        
+        NSString * address = [dict objectForKey:@"address"];
+        if (address != nil) {
+            theRestaurant.restAddress = address;
+        } else {
+            theRestaurant.restAddress = @"";
+            NSLog(@"I could not parse the restaurant address");
+        }
+        
+        NSString * city = [dict objectForKey:@"city"];
+        if (city != nil) {
+            theRestaurant.restCity = city;
+        } else {
+            theRestaurant.restCity = @"";
+            NSLog(@" I could not parse the restaurant city");
+        }
+        
+        NSString * state = [dict objectForKey:@"state"];
+        if (state != nil) {
+            theRestaurant.restState = state;
+        } else {
+            theRestaurant.restState = @"";
+            NSLog(@"I could not parse the restaurant state");
+        }
+        
+        NSString * zip = [dict objectForKey:@"zip"];
+        if (zip != nil) {
+            theRestaurant.restZip = zip;
+        } else {
+            theRestaurant.restZip = @"";
+            NSLog(@"I could not parse the restaurant zipcode");
+        }
+        
+        NSNumber * latitude = [dict objectForKey:@"latitude"];
+        if (latitude != nil) {
+            theRestaurant.restLatitude = latitude;
+        } else {
+            theRestaurant.restLatitude = [NSNumber numberWithInteger:0];
+            NSLog(@"I could not parse the restaurant latitude");
+        }
+        
+        NSNumber * longitude = [dict objectForKey:@"longitude"];
+        if (longitude != nil) {
+            theRestaurant.restLongitude = longitude;
+        } else {
+            theRestaurant.restLongitude = [NSNumber numberWithInteger:0];
+            NSLog(@"I could not parse the restaurant longitude");
+        }
+        
+        NSString * menuName = [dict objectForKey:@"menuName"];
+        if (menuName != nil) {
+            theRestaurant.restMenu = menuName;
+        } else {
+            theRestaurant.restMenu = @"";
+            NSLog(@"I could not parse the restaurant menu");
+        }
+        
+        NSArray * dishArray = [dict objectForKey:@"dishes"];
+        
+        for (NSDictionary *dishesDict in dishArray) {
+            
+            Dishes * theDish = [[Dishes alloc] init];
+            
+            NSString * dishName = [dishesDict objectForKey:@"name"];
+            if (dishName != nil) {
+                theDish.dishName = dishName;
+            } else {
+                theDish.dishName = @"";
+                NSLog(@"I could not parse the dish name");
+            }
+            
+            NSString * dishDesc = [dishesDict objectForKey:@"description"];
+            if (dishDesc != nil) {
+                theDish.dishDescription = dishDesc;
+            } else {
+                theDish.dishDescription = @"";
+                NSLog(@"I could not parse the dish description");
+            }
+            
+            NSString * dishImage = [dishesDict objectForKey:@"imageNameString"];
+            if (dishImage != nil) {
+                theDish.imageNameString = dishImage;
+            } else {
+                theDish.imageNameString = @"";
+                NSLog(@"I could not parse the dish image string");
+            }
+            
+            NSNumber * dishRating = [dishesDict objectForKey:@"rating"];
+            if (dishRating != nil) {
+                theDish.dishRating = dishRating;
+            } else {
+                theDish.dishRating = [NSNumber numberWithInteger:0];
+                NSLog(@"I could not parse the dish rating");
+            }
+            
+            NSNumber * dishPrice = [dishesDict objectForKey:@"price"];
+            if (dishPrice != nil) {
+                theDish.dishPrice = dishPrice;
+            } else {
+                theDish.dishPrice = [NSNumber numberWithInteger:0];
+                NSLog(@"I could not parse the dish price");
+            }
+            
+            [theRestaurant.dishesArray addObject:theDish];
+        } [self.restaurantArray addObject:theRestaurant];
+    }
+    
+}
 
 @end
