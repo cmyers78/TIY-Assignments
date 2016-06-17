2b4a53ebf34916c08713141cc5e657f231bec561
Chris Myers <cmyers78@gmail.com>
2016-06-16 22:07:38 -0600

    1st Commit - parse JSON function completed.

---
+++ 19/SpotifIron Swift Myers/SpotifIron Swift Myers/ViewController.swift
@@ -0,0 +1,115 @@
+//
+//  ViewController.swift
+//  SpotifIron Swift Myers
+//
+//  Created by Christopher Myers on 6/16/16.
+//  Copyright © 2016 Dragoman Developers, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let session = NSURLSession.sharedSession()
+    
+    var artistArray = [Artist]()
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    // MARK : Table View Methods
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView .dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        
+        return cell
+    }
+    
+    
+    
+    
+    // MARk: Parsing Method
+    
+    func fetchArtist (query : String) {
+        
+        let artistURLString = "https://api.spotify.com/v1/search?q=\(query)&type=artist"
+        
+        if let url = NSURL(string: artistURLString)
+        {
+            let task = self.session.dataTaskWithURL(url, completionHandler:
+            {
+                (data, response, error) in
+                
+                if error != nil
+                {
+                    print("An error occured")
+                    return
+                }
+                
+                do
+                {
+                    if let data = data {
+                        if let dict = try NSJSONSerialization.JSONObjectWithData(data, options: []) as? JSONDictionary {
+                            
+                            if let artistDict = dict["artists"] as? JSONDictionary {
+                                
+                                if let itemsArray = artistDict["items"] as? JSONArray {
+                                    
+                                    for itemsDict in itemsArray  {
+                                        
+                                        let theArtist = Artist()
+                                        
+                                        if let name = itemsDict["name"] as? String {
+                                            theArtist.name = name
+                                            
+                                            
+                                        } else {
+                                            print("I could not parse the name")
+                                        }
+                                        
+                                       self.artistArray.append(theArtist)
+                                    }
+                                } else {
+                                    print("I could not parse the items")
+                                }
+                            } else {
+                                print ("I could not parse the artists")
+                            }
+                        } else {
+                            print ("I could not parse the json dictionary")
+                        }
+                    }
+                } catch {
+                    // error happened
+                }
+                
+            })
+            task.resume()
+            
+        }
+    }
+
+
+}
+
+
