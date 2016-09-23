efddde3f342f45a5874ef8c4550e69bdd990a4f6
Chris Myers <cmyers78@gmail.com>
2016-09-23 14:01:12 -0400

    where to go from here?

--- TestingImages/TestingImages/TestingImages/ViewController.m
+++ TestingImages/TestingImages/TestingImages/ViewController.m
@@ -14,6 +14,7 @@
 
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 
+@property (weak, nonatomic) IBOutlet UILabel *imageNameLabel;
 
 @end
 
@@ -113,6 +114,9 @@
     NSString * myURLString = [retrieveURL absoluteString];
     
     NSLog(myURLString);
+    
+    self.imageNameLabel.text = myURLString;
+    
     
     
 }
